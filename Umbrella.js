fd979baf1401889a839ff7349bf2525853ebc8e5
SanWHJGitHub <76816854+SanWHJGitHub@users.noreply.github.com>
2021-03-26 19:11:14 +0530

    Update Umbrella.js

--- Umbrella.js
+++ Umbrella.js
@@ -7,7 +7,7 @@ class Umbrella
             isStatic:true 
         }
 
-        this.image = loadImage('Walking Frame/walking_1.png');
+        this.image = loadImage('walking_1.png');
         this.umbrella = Bodies.circle(x,y,50,options);
         this.radius = 50;
         World.add(world, this.umbrella)
